7b669de141e68d2039a3c4bf6bc3e32d1274d047
zaidkhan0997 <kzaid0997@gmail.com>
2021-10-11 19:08:48 +0530

    sweet: BringUp PEX Official
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ aosp_sweet.mk
@@ -0,0 +1,29 @@
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/sweet/device.mk)
+
+# Inherit common PixelExtended configurations
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# PixelExtended Official Stuff
+PEX_BUILD_TYPE := OFFICIAL
+PEX_MAINTAINER := zaidkhan0997
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+PRODUCT_NAME := aosp_sweet
+PRODUCT_DEVICE := sweet
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 10 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
