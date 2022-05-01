b71ce6aeee25f17bb840c6ab29b8901f3737a031
zaidkhan0997 <kzaid0997@gmail.com>
2022-05-01 12:57:06 +0530

    sweet: Bringup PixelExtended OFFICIAL
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,10 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExtended stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# PixelExtended Official Stuff
+PEX_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := aosp_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
