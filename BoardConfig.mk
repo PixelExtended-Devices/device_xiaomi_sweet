5dadad57bb43eeff45126487aaca606eb0261134
zaidkhan0997 <kzaid0997@gmail.com>
2022-05-02 13:45:36 +0530

    sweet: ignore neverallows
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,7 @@ TARGET_SCREEN_DENSITY := 440
 
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Inherit from proprietary files
 include vendor/xiaomi/sweet/BoardConfigVendor.mk
